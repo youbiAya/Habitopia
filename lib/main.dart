88f5d5bc924030dbc1753402148daa800af16797
Youbi Aya <youbiy566@gmail.com>
2024-12-24 10:27:31 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(HabitAdapter());
+  await Hive.openBox('themeBox');
   var boxHabit = await Hive.openBox<Habit>('habitBox');
   print('Habits before running app: ${boxHabit.values.toList()}');
 
--- lib/theme/theme_provider.dart
+++ lib/theme/theme_provider.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class ThemeProvider extends ChangeNotifier {
+  static const _themeBoxKey = 'themeBox';
+  static const _themeModeKey = 'themeMode';
   ThemeMode _themeMode = ThemeMode.light;
+
+  ThemeProvider() {
+    _loadThemeFromStorage();
+  }
 
   ThemeMode get themeMode => _themeMode;
 
   void toggleTheme() {
     _themeMode = _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    _saveThemeToStorage();
     notifyListeners();
+  }
+
+  void _loadThemeFromStorage() {
+    final box = Hive.box(_themeBoxKey);
+    final savedTheme = box.get(_themeModeKey, defaultValue: 'light');
+    _themeMode = savedTheme == 'dark' ? ThemeMode.dark : ThemeMode.light;
+  }
+
+  void _saveThemeToStorage() {
+    final box = Hive.box(_themeBoxKey);
+    box.put(_themeModeKey, _themeMode.name);
   }
 }
